7ffbfb27e1001e9eb6baf33251b7228414a9b6b4
ZY <1035041075@qq.com>
2019-12-01 00:24:16 +0800

    :construction: loading animation wip

---
+++ src/pages/project/operation/components/LoadAnimation.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './LoadAnimate.less';
+
+const LoadAnimate = () => {  
+  return <div className={styles['load-animate']}></div>;
+};
+
+export default LoadAnimate;
--- src/pages/project/operation/index.tsx
+++ src/pages/project/operation/index.tsx
@@ -1,5 +1,5 @@
-// import { Avatar, List, Statistic } from 'antd';
-import React from 'react';
+// import { Avatar, List, Statistic, Divider } from 'antd';
+import React, { useLayoutEffect } from 'react';
 
 import classNames from 'classnames/bind';
 import { Row, Col } from 'antd';
@@ -8,23 +8,33 @@ import LineChart from '../components/BlockChart/LineChart';
 
 const cx = classNames.bind(styles);
 
-const data = Array.from({ length: 6 }).map(() =>
-  Array.from({ length: 60 })
+const data = Array.from({ length: 6 }).map((_, index) => ({
+  title: `cpu${index}负载`,
+  list: Array.from({ length: 60 })
     .map((_, i) => i)
     .reduce((acc, _, index) => [...acc, acc[index] + 10 * (Math.random() - 0.5)], [0])
     .map((item, index) => ({ date: index, value: item })),
-);
+}));
 
-const Operation = () => (
-  <div className={cx('wrapper')}>
-    <Row gutter={24}>
-      {data.map(item => (
-        <Col style={{ marginBottom: 10 }} key={item.toString()} lg={4} md={6} xs={12}>
-          <LineChart title="cpu 负载" data={item}></LineChart>
-        </Col>
-      ))}
-    </Row>
-  </div>
-);
+
+const Operation = () => {
+
+  // useLoadAnimate();
+
+  return (
+    <div className={cx('wrapper')}>
+      <Row gutter={24}>
+        {data.map((item, index) => (
+          <Col style={{ marginBottom: 10 }} key={item.title} lg={4} md={6} xs={12}>
+            <LineChart title={item.title} data={item.list}></LineChart>
+          </Col>
+        ))}
+      </Row>
+      <Row>
+        <Col xs={18}></Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Operation;
